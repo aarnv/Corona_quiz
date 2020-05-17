520d688f5415c8c1af9a3b1d6765e84119152600
aarnv <65150142+aarnv@users.noreply.github.com>
2020-05-17 19:20:39 +0530

    Add files via upload

---
+++ lib.py
@@ -0,0 +1,45 @@
+#survey
+
+
+print("The most common symptoms of COVID-19 are ___________,__________,_____________.")
+guess1 = input("Enter the first common symptom:")
+guess2 = input("Enter the second common symptom:")
+guess3 = input("Enter the third common symptom:")
+print("The most common symptom of COVID-19 are: " + guess1 + "," + guess2 + "," + guess3 + ".")
+
+
+print("\n")
+print("__________________________________________________________________________________________________________________________________________________________________________________________________________________________")
+print("\n")
+
+
+
+print("The serious symptom are ____________,___________,_____________.")
+symptom1 = input("Enter the first symptom:")
+symptom2 = input("Enter the second symptom: ")
+sypmtom3 = input("Enter the third symptom: ")
+print("The serious symptoms are: " + symptom1 + "," + symptom2 + "," + sypmtom3)
+
+print("\n")
+print("___________________________________________________________________________________________________________________________________________________________________________________________________________________________")
+print("\n")
+
+print("Clean your hands by__________,_________,_________")
+thing1 = input("Enter the answer for the first blank: ")
+thing2 = input("Enter the answer for the second blank: ")
+thing3 = input("Enter the answer for the third blank: ")
+
+print("Clean your hand by:  " + thing1 + "," + thing2 + "," + thing3 + ".")
+print("\n")
+print("_________________________________________________________________________________________________________________________________________________________________________________________________________________")
+print("\n")
+
+
+print(" STAY HOME. SAVE LIVES")
+print("Help stop coronavirus")
+print("1. STAY home")
+print("2. KEEP a safe distance")
+print("3. WASH hands often")
+print("COVER your cough")
+print("SICK?Call the helpline")
+
